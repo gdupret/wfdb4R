6a4494a5e6ab5e900e45c88aa9376a3acba74f5d
Georges Dupret <georges.dupret@gmail.com>
2017-01-03 08:58:45 -0800

    sync

--- R/getvec.R
+++ R/getvec.R
@@ -1,7 +1,7 @@
 #' read a physionet signal from file
 #'
 #' @param filename The path to the file to read, with or without extension
-#' @note The C function in wfdb must be called from the directory where the file resides. This wrapper make sure to move to the directory first, reads the signal and return to the original working directory
+#' @note The C function in wfdb must be called from the directory where the file resides. This wrapper make sure to move to the directory first, reads the signal and return to the original working directory. In other words, its usage is more in line with what you would normally expect.
 #' @return a Matrix with one signal per row
 #' @export
 getvec <- function(filename){
